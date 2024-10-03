c6ed769e0d7f640c3283ec27d3bfbbd8cf9e60ce
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-10-03 10:56:16 +0200

    I have itroduce SearchQueryDTO so I can implement Validation in search bar function

---
+++ Shared-Layer/DTO´s/SearchQuery/SearchQueryDTO.cs
@@ -0,0 +1,12 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared_Layer.DTO_s.SearchQuery
+{
+    public class SearchQueryDTO
+    {
+        [StringLength(30, ErrorMessage = "Search query cannot exceed 30 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Search query can only contain letters and spaces.")]
+        public string SearchQuery { get; set; } = string.Empty; // Default to an empty string
+    }
+}
