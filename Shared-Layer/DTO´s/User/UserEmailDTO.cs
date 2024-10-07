9ad57f457052eb58d65761a21d679777dad3852f
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-10-07 15:13:49 +0200

    new data annotation is added to UserEmailDTO

--- Shared-Layer/DTO´s/User/UserEmailDTO.cs
+++ Shared-Layer/DTO´s/User/UserEmailDTO.cs
@@ -6,6 +6,7 @@ namespace Shared_Layer.DTO_s.User
     public class UserEmailDTO
     {
         [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.EmailAddress)]
         [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
     }
