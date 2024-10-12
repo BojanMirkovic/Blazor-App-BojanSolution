3a7c4a86aa56802016fa1197e013bd07d023c84e
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-10-12 20:40:38 +0200

    UserIdDTO is updated

--- Shared-Layer/DTO´s/User/UserIdDTO.cs
+++ Shared-Layer/DTO´s/User/UserIdDTO.cs
@@ -5,7 +5,7 @@ namespace Shared_Layer.DTO_s.User
     public class UserIdDTO
     {
         [Required(ErrorMessage = "User ID is required.")]
-        [RegularExpression(@"^[A-Fa-f0-9\-]{36}$", ErrorMessage = "The ID format is incorrect.")]
+        [RegularExpression(@"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", ErrorMessage = "The ID format is incorrect.")]
         public string Id { get; set; }
     }
 }
